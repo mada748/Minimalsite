54b55c12a2e3ba22766abf829ebf61e921646eb0
StoneCode <mattiacoountpersonalr@gmail.com>
2025-10-11 11:28:46 +0200

    Update script.js

--- script.js
+++ script.js
@@ -12,15 +12,17 @@ const endB = 255;
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
 
-
+    
     let ratio = Math.min(scrollY / SCROLL_DISTANCE, 1);
     
+
     const r = Math.round(startR + (endR - startR) * ratio);
     const g = Math.round(startG + (endG - startG) * ratio);
     const b = Math.round(startB + (endB - startB) * ratio);
 
-    
+   
     const newColor = `rgb(${r}, ${g}, ${b})`;
+    
     
     document.documentElement.style.setProperty('--bg-color', newColor);
 });
