3dd334a633dcda1f6e893c2e5d97a84a47f11ca3
StoneCode <mattiacoountpersonalr@gmail.com>
2025-10-11 11:31:38 +0200

    Update script.js

--- script.js
+++ script.js
@@ -1,29 +1,26 @@
 
 const SCROLL_DISTANCE = 700; 
 
-const startR = 0;
-const startG = 0;
-const startB = 0;
+const startR = 100;
+const startG = 100;
+const startB = 100;
 
-const endR = 178;
-const endG = 221;
-const endB = 255; 
+const endR = 0;
+const endG = 0;
+const endB = 0; 
 
 window.addEventListener('scroll', () => {
     const scrollY = window.scrollY;
 
-    
     let ratio = Math.min(scrollY / SCROLL_DISTANCE, 1);
     
-
     const r = Math.round(startR + (endR - startR) * ratio);
     const g = Math.round(startG + (endG - startG) * ratio);
     const b = Math.round(startB + (endB - startB) * ratio);
 
-   
     const newColor = `rgb(${r}, ${g}, ${b})`;
     
-    
+
     document.documentElement.style.setProperty('--bg-color', newColor);
 });
 
