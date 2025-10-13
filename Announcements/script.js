593cbfaf7c5311dd8c7df54d97cedfd41c510939
StoneCode <mattiacoountpersonalr@gmail.com>
2025-10-13 17:04:00 +0200

    Create script.js

---
+++ Announcements/script.js
@@ -0,0 +1,14 @@
+document.getElementById('suggestion-form').addEventListener('submit', function(event) {
+    event.preventDefault(); 
+
+    const repoOwner = 'mada748'; 
+    const repoName = 'MinimalBrowser';      
+    const title = encodeURIComponent(document.getElementById('suggestion-title').value);
+    const body = encodeURIComponent(document.getElementById('suggestion-body').value);
+
+
+    const url = `https://github.com/${repoOwner}/${repoName}/issues/new?title=${title}&body=${body}&labels=suggestion`;
+
+
+    window.open(url, '_blank');
+});
